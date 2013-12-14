2b0f2ea8001e68bac523a8725198cc7e1914f7df
Shiz0 <Shizik32@gmail.com>
2013-12-15 00:52:47 +0400

    Долгожданное отображение повреждений героя

--- hud.py
+++ hud.py
@@ -2,7 +2,101 @@ __author__ = 'Ecialo'
 import cocos
 import consts as con
 import expression as ex
+
+from cocos.layer import Layer
+from cocos.director import director
+from cocos.batch import BatchNode
+from cocos.sprite import Sprite
+from pyglet.event import EventDispatcher
+
 consts = con.consts
+
+
+class ResizableLayer(Layer, EventDispatcher):
+    is_event_handler = True
+
+    def __init__(self):
+        super(ResizableLayer, self).__init__()
+        self.cur_x, self.cur_y = director.get_window_size()
+        self.dx, self.dy = 0, 0
+
+    def on_resize(self, width, height):
+        self.dx = width - self.cur_x
+        self.dy = height - self.cur_y
+        self.cur_x, self.cur_y = width, height
+        self.dispatch_event('on_hud_shift_needed', self.dx, self.dy)
+
+ResizableLayer.register_event_type('on_hud_shift_needed')
+
+
+class DudeDamageLayer(ResizableLayer):
+    is_event_handler = True
+
+    def __init__(self, game_layer):
+        super(DudeDamageLayer, self).__init__()
+        self.game_layer = game_layer
+        self.red_bp = BatchNode()
+        self.normal_bp = BatchNode()
+        self.armored_bp = BatchNode()
+
+        self.ui_body = {'back': {}, 'front': {}, 'armored': {}}
+        self._init_body_parts('back')
+        self._init_body_parts('front')
+        self._init_body_parts('armored')
+
+        # the layer now listens to on_take_damage event from hero
+        self.game_layer.hero.push_handlers(self.on_take_damage)
+
+        for body_part in self.ui_body['back']:
+            self.red_bp.add(self.ui_body['back'][body_part])
+        for body_part in self.ui_body['front']:
+            self.normal_bp.add(self.ui_body['front'][body_part])
+        for body_part in self.ui_body['armored']:
+            self.armored_bp.add(self.ui_body['armored'][body_part])
+
+        self.add(self.red_bp, z=-1)
+        self.add(self.normal_bp, z=0)
+        self.add(self.armored_bp, z=1)
+
+    def _init_body_parts(self, layer_type='front'):
+        # ACHTUNG! HARDCODE!
+        self.ui_body[layer_type]['head'] = Sprite(consts['hud']['body'][layer_type]['head'])
+        self.ui_body[layer_type]['head'].position = (50, self.cur_y-50)
+        self.ui_body[layer_type]['chest'] = Sprite(consts['hud']['body'][layer_type]['chest'])
+        self.ui_body[layer_type]['chest'].position = (50, self.cur_y-85)
+        self.ui_body[layer_type]['left_arm'] = Sprite(consts['hud']['body'][layer_type]['left_arm'])
+        self.ui_body[layer_type]['left_arm'].position = (34, self.cur_y-90)
+        self.ui_body[layer_type]['right_arm'] = Sprite(consts['hud']['body'][layer_type]['right_arm'])
+        self.ui_body[layer_type]['right_arm'].position = (66, self.cur_y-91)
+        self.ui_body[layer_type]['legs'] = Sprite(consts['hud']['body'][layer_type]['legs'])
+        self.ui_body[layer_type]['legs'].position = (50, self.cur_y-119)
+
+    def on_hud_shift_needed(self, dx, dy):
+        for layer_type in ('back', 'front', 'armored'):
+            for body_part in self.ui_body[layer_type]:
+                self.ui_body[layer_type][body_part].y += dy
+
+    def on_take_damage(self, body_part):
+        armor_damage = False
+        if body_part.slot >= con.ARMOR:
+            slot = body_part.slot - con.ARMOR
+            armor_damage = True
+        else:
+            slot = body_part.slot
+
+        if slot == con.HEAD:
+            bp_name = 'head'
+        elif slot == con.CHEST:
+            bp_name = 'chest'
+        elif slot == con.LEGS:
+            bp_name = 'legs'
+        else:
+            bp_name = 'right_arm'
+
+        if armor_damage:
+            self.ui_body['armored'][bp_name].opacity = 255.0 * body_part.armor / body_part.max_armor
+        else:
+            self.ui_body['front'][bp_name].opacity = 255.0 * body_part.health / body_part.max_health
 
 
 class HUD(cocos.layer.Layer):
@@ -14,6 +108,7 @@ class HUD(cocos.layer.Layer):
         super(HUD, self).__init__()
         self.game_layer.script_manager.set_handler('run_dialog', self.run_dialog)
         self.current_dialog = None
+        self.add(DudeDamageLayer(game_layer))
 
     def run_dialog(self, dialog_name, actor, location):
         print "DIALOGS"
@@ -33,6 +128,3 @@ class HUD(cocos.layer.Layer):
 
     def on_exit(self):
         self.game_layer.script_manager.remove_handler('run_dialog', self)
-
-
-
