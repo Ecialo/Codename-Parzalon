39e499d8aa5f0734a85a5a91dae9ca6eee1b38a4
Ecialo <alextby@mail.ru>
2013-12-24 23:55:08 +0400

    Исправлен скрипт для создания нарезки

--- setup_cutting.py
+++ setup_cutting.py
@@ -5,4 +5,4 @@ setup(
         version = "0.1",
         description = "Sample cx_Freeze PyQt4 script",
         options = {"build_exe" : {"include_files" : ['draw_texture.png']}},
-        executables = [Executable("codename-Parzalon.py")])
+        executables = [Executable("cutting.py")])
