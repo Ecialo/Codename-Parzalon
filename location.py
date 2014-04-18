f061e1f2ec8dae175cb69772d9b5711018c13843
Ecialo <alextby@mail.ru>
2014-04-18 20:09:10 +0400

    Тайлы объединены в блоки Вертикальные от 2 тайлов высотой  и горизонтальные

--- location.py
+++ location.py
@@ -239,12 +239,12 @@ class Location_Layer(layer.ScrollableLayer):
 
     def _create_b2_tile_map(self, rect_map):
 
-        def try_create_and_append_block(cells_in_block):
-            #num_of_cells = len(cells_in_block)
-            #checker = (num_of_cells > 0) if mode else (num_of_cells > 1)
-            if cells_in_block:
-                height = 1 # len(cells_in_block) if not mode else 1
-                width = len(cells_in_block) # if mode else 1
+        def try_create_and_append_block(cells_in_block, mode):
+            num_of_cells = len(cells_in_block)
+            checker = (num_of_cells > 0) if mode else (num_of_cells > 1)
+            if checker:
+                height = len(cells_in_block) if not mode else 1
+                width = len(cells_in_block) if mode else 1
                 half_height = height/2.0
                 half_width = width/2.0
                 #highest_cell = cells_in_block[-1]
@@ -254,9 +254,6 @@ class Location_Layer(layer.ScrollableLayer):
                 shape.SetAsBox(half_width, half_height, (cx, cy), NO_ROTATION)
                 self.b2level.CreateFixture(shape=shape, userData=cell)
                 self.b2level.fixtures[-1].filterData.categoryBits = con.B2SMTH | con.B2LEVEL
-                return True
-            else:
-                return False
 
         # WIDTH, HEIGHT = con.TILE_SIZE/2, con.TILE_SIZE/2
         cells = rect_map.cells
@@ -266,21 +263,21 @@ class Location_Layer(layer.ScrollableLayer):
         shape = b2.b2PolygonShape()
         #print "TEST"
         #i = 0
-        # for cell_column in cells:
-        #     cells_in_vertical_block = []
-        #     for cell in cell_column:
-        #         if cell.get('top'):
-        #             #print i
-        #             cells_in_vertical_block.append(cell)
-        #             #print self.b2world
-        #             #self.b2level.fixtures[-1].maskBits = con.B2EVERY
-        #             #if i>9990:
-        #             #   temp = self.b2world
-        #         else:
-        #             #print len(cells_in_block)
-        #             if try_create_and_append_block(cells_in_vertical_block, 0):
-        #                 cells_in_vertical_block = []
-        #     try_create_and_append_block(cells_in_vertical_block, 0)
+        for cell_column in cells:
+            cells_in_vertical_block = []
+            for cell in cell_column:
+                if cell.get('top'):
+                    #print i
+                    cells_in_vertical_block.append(cell)
+                    #print self.b2world
+                    #self.b2level.fixtures[-1].maskBits = con.B2EVERY
+                    #if i>9990:
+                    #   temp = self.b2world
+                else:
+                    #print len(cells_in_block)
+                    try_create_and_append_block(cells_in_vertical_block, 0)
+                    cells_in_vertical_block = []
+            try_create_and_append_block(cells_in_vertical_block, 0)
 
         for j in xrange(n):
             cells_in_horizontal_block = []
@@ -289,9 +286,9 @@ class Location_Layer(layer.ScrollableLayer):
                 if cell.get('top'):
                     cells_in_horizontal_block.append(cell)
                 else:
-                    if try_create_and_append_block(cells_in_horizontal_block):
-                        cells_in_horizontal_block = []
-            try_create_and_append_block(cells_in_horizontal_block)
+                    try_create_and_append_block(cells_in_horizontal_block, 1)
+                    cells_in_horizontal_block = []
+            try_create_and_append_block(cells_in_horizontal_block, 1)
 
 
 
