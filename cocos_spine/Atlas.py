b16159c909b9de3f3a66192f621a21c22dacca39
Ecialo <alextby@mail.ru>
2014-04-26 17:54:19 +0400

    Поправлена работа атласа

--- cocos_spine/Atlas.py
+++ cocos_spine/Atlas.py
@@ -11,7 +11,9 @@ class Atlas(object):
     def __init__(self, file):
         self.atlas = pyglet.image.atlas.TextureBin(1024, 1024)
         self.lib = {}
+        self.region_lib = {}
         self.load(file)
+        self.preprocess()
 
     def load(self, file):
         if not file:
@@ -31,6 +33,7 @@ class Atlas(object):
             region_data = {}
             for line in fh:
                 value = clean(line)
+                #print value, state
                 while True:
                     if state is st_filename:
                         file_name = value
@@ -38,8 +41,10 @@ class Atlas(object):
                         state = st_metadata
                     elif state is st_metadata:
                         if ':' in value:
-                            name, data = map(clean, value.split(':'))
-                            file_data[name] = data
+                            #name, data = map(clean, value.split(':'))
+                            #file_data[name] = data
+                            #print name, data
+                            pass
                         else:
                             state = st_name
                             continue
@@ -47,9 +52,12 @@ class Atlas(object):
                         if value:
                             region_name = value
                             state = st_data
+                            region_data = {}
                         else:
                             if file_name:
                                 self.lib[file_name] = file_data
+                                file_data = {}
+                                region_data = {}
                             state = st_filename
                     elif state is st_data:
                         if ':' in value:
@@ -62,6 +70,20 @@ class Atlas(object):
                             state = st_name
                             continue
                     break
+        region_data['region'] = self.get_region(image, region_data)
+        file_data[region_name] = region_data
+        self.lib[file_name] = file_data
+
+    def preprocess(self):
+        for file_data in self.lib.itervalues():
+            for region_name in file_data:
+                #print region_name
+                #print file_data[region_name]
+                self.region_lib[region_name] = file_data[region_name]['region']
+        #print self.region_lib['tail05']
+
+    def get_attachment_region(self, name):
+        return self.region_lib[name]
 
     def get_region(self, image, region_data):
         x, y = region_data['xy']
