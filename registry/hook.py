4f38a795061aabefba608e4894feecb1b34a4035
ecialo <alextby@mail.ru>
2014-06-01 00:29:41 +0400

    WIP

--- registry/hook.py
+++ registry/hook.py
@@ -9,8 +9,30 @@ __author__ = 'Ecialo'
 
 class Lazy_Resource_Registry(object):
 
-    def __init__(self, name, loader):
+    def __init__(self):
         self._data = {}
+        self.index()
+
+    def __getitem__(self, item):
+        if type(self._data[item]) is not str:
+            return self._data[item]
+        else:
+            self.load_then_apply(item)
+            return self._data[item]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+
+    def load_then_apply(self, item):
+        tmp = None
+        querry = "from .%s import *" % item
+        exec querry
+        exec "tmp = %s" % item
+        self[item] = tmp
+
+    def index(self):
+        import os
+
 
 
 def load_effects():
